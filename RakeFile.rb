d0bfba29ad9f6efda828fa2b449fc133bc41ce13
George Mauer <gmauer@gmail.com>
2009-11-02 16:00:36 -0600

    fixed build file

--- RakeFile.rb
+++ RakeFile.rb
@@ -7,7 +7,7 @@ DOT_NET_PATH = "#{ENV["SystemRoot"]}\\Microsoft.NET\\Framework\\v3.5\\"
 COMPILE_TARGET = 'Release'
 CORE_PROJECT_PATH = "app\\#{PROJECT_NAME}\\"
 TEST_PROJECT_PATH = "app\\#{TEST_PROJECT_NAME}\\"
-OUTPUT_PATH = "bin\\"
+OUTPUT_PATH = "build\\"
 DONT_OUTPUT = /^(.*\.pdb)|(log4net\.config)$/
 TEST_RUNNER = "tools\\test_runners\\nunit-console.exe"
 
@@ -16,7 +16,7 @@ task :default => [:test_build,:output_core]
 
 def compile(projectFile, compileTarget = COMPILE_TARGET)
 	msBuild = "#{DOT_NET_PATH}\msbuild.exe"
-	sh "#{msBuild} /p:Configuration=Release #{projectFile} /t:Rebuild"
+	sh "#{msBuild} /p:Configuration=#{compileTarget} #{projectFile} /t:Rebuild"
 end
 
 desc 'Clean output directory'
@@ -27,12 +27,16 @@ end
 task :build_core => :clean do
 	compile("#{CORE_PROJECT_PATH}GIM.Quantities.csproj", COMPILE_TARGET)
 end
+
+desc 'Output build'
 task :output_core => :build_core do
 	source_path = "#{CORE_PROJECT_PATH}bin\\#{COMPILE_TARGET}\\"
 	ignore = DONT_OUTPUT
 	files = Dir.new(source_path).entries.find_all {|s| (s=~/[a-zA-Z]/) and !(s=~ ignore)} #/
 	files.each {|f| File.move source_path+f, OUTPUT_PATH }
 end
+
+desc 'Run Tests'
 task :test_build => :build_core do
   compile("#{TEST_PROJECT_PATH}\\#{TEST_PROJECT_NAME}.csproj", 'Debug') 
   tests = "#{TEST_PROJECT_PATH}\\bin\\Debug\\#{TEST_PROJECT_NAME}.dll"
