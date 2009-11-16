f80243cebdf6a52ca3b6e2cde09694f48e9974dd
George Mauer <gmauer@gmail.com>
2009-11-15 23:43:03 -0600

    Added first pass conversion of mass

---
+++ app/GIM.Quantities.Tests/Conversion/Mass_conversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GIM.Quantities.Tests.Conversion {
+    public class Mass_conversion {
+        [Test]
+        public void Can_explicitly_convert_kgs_to_lbs() {
+            1.Kilograms().In(MassUnit.Pounds).ShouldEqualToPrecision(2.20462262.Pounds(), 8);
+        }
+        [Test]
+        public void Can_explicitly_convert_lbs_to_kgs() { 
+            1.Pounds().In(MassUnit.Kilograms).ShouldEqualToPrecision(0.45359237.Kilograms(), 8);
+        }
+        [Test]
+        public void Can_explicitly_convert_kilograms_to_kilograms() {
+            3.Kilograms().In(MassUnit.Kilograms).ShouldEqual(3.Kilograms());
+        }
+        [Test]
+        public void Can_explicitly_convert_kgs_to_lbs_with_extension() {
+            1.Kilograms().In(x=>x.Pounds()).ShouldEqualToPrecision(2.20462262.Pounds(), 8);
+        }
+    }
+}
---
+++ app/GIM.Quantities/Conversion/MassConversions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GIM.Quantities.Conversion {
+    public class MassConversion {
+        const double _lbs_In_1_Kg = 2.20462262;
+        private MassConversion() { }
+        public Mass Convert(Mass from, MassUnit to) {
+            if (from.Unit.Equals(to))
+                return new Mass(from.Amount, to);
+
+            if (from.Unit.Equals(MassUnit.Kilograms) && to.Equals(MassUnit.Pounds))
+                return new Mass(from.Amount * _lbs_In_1_Kg, to);
+            if (from.Unit.Equals(MassUnit.Pounds) && to.Equals(MassUnit.Kilograms))
+                return new Mass(from.Amount / _lbs_In_1_Kg, to);
+            throw new NotImplementedException();
+        }
+        private static MassConversion _instance = new MassConversion();
+        public static MassConversion Instance {
+            get { return _instance; }
+        }
+    }
+}
