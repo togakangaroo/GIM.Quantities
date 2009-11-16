1c385fbb65eb0f0654bccf2de4bcc81e7b321657
George Mauer <gmauer@gmail.com>
2009-11-15 23:08:00 -0600

    Apparently the previous behavior of not throwing DivideByZeroException was following the IEEE 754 arithmetic standard, so removed the tests for it

--- app/GIM.Quantities.Tests/Arithmetic/Divide.cs
+++ app/GIM.Quantities.Tests/Arithmetic/Divide.cs
@@ -13,12 +13,8 @@ namespace GIM.Quantities.Tests.Arithmetic {
             (30.Pounds() / 10.Pounds()).ShouldEqual(3);
         }
         [Test]
-        public void Cant_divide_mass_by_zero() {
-            Assert.Throws<DivideByZeroException>(delegate{ var _=30.Pounds() / 0.Pounds();});
-        }
-        [Test]
-        public void Cant_divide_volume_by_zero() {
-            Assert.Throws<DivideByZeroException>(delegate { var _ = 30.Liters() / 0.Liters(); });
+        public void Numerator_can_be_zero() {
+            (0.Pounds() / 10.Pounds()).ShouldEqual(0);
         }
     }
 }
